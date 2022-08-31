5ecfbae63f2f02e11f0911832c24ef63e6fc1de9
hkw <MSI@DESKTOP-S3M0BRN>
2022-08-31 21:11:53 +0900

    sdsdsd

--- Teamroject_Javafx/src/Exaa.java
+++ Teamroject_Javafx/src/Exaa.java
@@ -1,4 +1,6 @@
 
 public class Exaa {
-
+public Exaa() {
+	// TODO Auto-generated constructor stub
+}
 }
---
+++ text02/src/text1/Ex01.java
@@ -0,0 +1,10 @@
+package text1;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!!");
+
+	}
+
+}
